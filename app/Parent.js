382b96c411bcb0221f41b3a5dd563c05a96f68f0
Irena Jurasek <irenaskydive@gmail.com>
2020-02-05 15:26:00 +0100

    Add Statefull Parent Component

---
+++ app/Parent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Parent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { name: 'Irena' }
+    }
+    render() {
+        return <div></div>;
+    }
+}
